971d22a1a195226b7111e78ac79eddd098a92a4f
The Goose <trevordammon@gmail.com>
2018-10-06 11:29:54 -0500

    Stretch Goals Complete

--- client.js
+++ client.js
@@ -11,8 +11,8 @@ function readyNow() {
             this.salary= parseFloat(salary).toFixed(2);
         }
     }
+
     let employees = [];
-    let totalMonthlySalary= 0;
     $('#addEmployee').on('click',function(){
         event.preventDefault();
         employees.push(new Employee($('#first').val(),$('#last').val(),$('#id').val(),$('#job').val(),$('#salary').val()));
@@ -20,8 +20,29 @@ function readyNow() {
         
         $('#table').empty();
         $('#table').append(`<tr><th>First</th><th>Last</th><th>ID</th><th>Position</th><th>Salary</th>`)
+        modifyTable();
+    });
+
+    $('#table').on('click', '#delete', function(){
+        console.log($(this).closest('tr').find("#code").text());
+        for(let index in employees){
+            if(employees[index].id == $(this).closest('tr').find("#code").text()){
+                employees.splice(index,1);
+                console.log(index)
+                
+            }
+        }
+        modifyTable();
+    })
+
+    function modifyTable() {
+        let totalMonthlySalary= 0;
+
+        $('#table').empty();
+        $('#table').append(`<tr><th>First</th><th>Last</th><th>ID</th><th>Position</th><th>Salary</th>`)
+
         for(let employee of employees){
-            $('#table').append(`<tr><td>${employee.first}</td><td>${employee.last}</td><td>${employee.id}</td><td>${employee.job}</td><td>$${employee.salary}</td></tr>`)
+            $('#table').append(`<tr><td>${employee.first}<button id="delete">X</button></td><td>${employee.last}</td><td id='code'>${employee.id}</td><td>${employee.job}</td><td>$${employee.salary}</td></tr>`)
             totalMonthlySalary+= Number((employee.salary/12).toFixed(2));
             console.log(totalMonthlySalary);
             $('#first').val( '' );
@@ -32,9 +53,9 @@ function readyNow() {
         }
         $('#table').append(`<tr><td colspan='5' id='left'><b>Total Monthly</b>: $${totalMonthlySalary}</td></tr>`)
         if(totalMonthlySalary > 20000){
-            $('.container').toggleClass("red");
-            document.body.style.backgroundColor = "tomato";
+            $('#left').toggleClass("red");
         }
-    });
+    }
+
 }
 
